0c0912e442d3b235c03978505e5ba015acddd732
Vadymshairan <95377527+Vadymshairan@users.noreply.github.com>
2022-08-07 07:14:20 +0300

    +

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,36 +1,46 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
-console.log(galleryItems);
-
-const galleryEl = document.querySelector('.gallery');
+// console.log(galleryItems);
 
 // 1. Создание и рендер разметки
-const galleryMarkupEl = galleryItems
-  .map(
-    item =>
-      `<div class="gallery__item"><a class="gallery__link" href="${item.original}" target="_blank" rel="noreferrer noopener"><img class="gallery__image"
-  src='${item.preview}'
-  alt='${item.description}'
-  data-source='${item.original}'
-/></div>`
-  )
-  .join('');
+const galleryEl = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryEl.insertAdjacentHTML('afterbegin', galleryMarkup);
 
-galleryEl.insertAdjacentHTML('afterbegin', galleryMarkupEl);
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`;
+    })
+    .join('');
+}
+console.log(galleryMarkup);
 
 //Реализация делегирования на div.gallery и получение url большого изображения.
-// const imageEl = document.querySelector('.gallery__image');
-// console.log(imageEl);
-galleryEl.addEventListener('click', getUrlOriginalImg);
 
-function getUrlOriginalImg(event) {
-  event.preventDefault();
-  if (event.target.nodeName !== 'IMG') {
+galleryEl.addEventListener('click', onImageClick);
+
+function onImageClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== 'IMG') {
     return;
   }
 
   //   console.log('event.target: ', event.target);
   //   console.log('event.currentTarget: ', event.currentTarget);
-  console.log(event.target.dataset.source);
+  // console.log(e.target.dataset.source);
+  const imgSourceUrl = e.target.dataset.source;
+  console.log(imgSourceUrl);
 }
