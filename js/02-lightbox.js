8eace5001fea963cba1016c7185bd5a17da072f4
Vadymshairan <95377527+Vadymshairan@users.noreply.github.com>
2022-08-07 16:30:21 +0300

    Задание 2 - библиотека SimpleLightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,7 +1,7 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
 
 // 1. Создание и рендер разметки
 const galleryEl = document.querySelector('.gallery');
@@ -13,8 +13,11 @@ function createGalleryMarkup(galleryItems) {
     .map(({ preview, original, description }) => {
       return `
       <a class="gallery__item" href="${original}">
-  <img class="gallery__image" src="${preview}" alt="${description}" />
-</a>`;
+        <img
+        class="gallery__image"
+        src="${preview}" 
+        alt="${description}"/>
+      </a>`;
     })
     .join('');
 }
